ba9405e4f03fd5ecab928c6d781af995ab168994
chrg <christian.ricci2000@gmail.com>
2025-10-10 20:52:04 +0200

    Add episode

---
+++ episode/luna.lua
@@ -0,0 +1,5 @@
+local smwMap = require("smwMap")
+
+-- Force the SMW costumes, 'cause why not
+Player.setCostume(CHARACTER_MARIO,"SMW-Mario",true)
+Player.setCostume(CHARACTER_LUIGI,"SMW-Luigi",true)
